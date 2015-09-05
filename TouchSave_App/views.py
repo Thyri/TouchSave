74e0e04059d9daa5b05b3ad7e82b0ece8bcb3a12
sklei2 <stk1151@rit.edu>
2015-09-05 15:40:46 -0400

    Trying to add a login page
    FINGERS CROSSED!!!

--- TouchSave_App/views.py
+++ TouchSave_App/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from ToolShare.models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
+from django.db import IntegrityError
 # Create your views here.
+def index(request):
+	template = "index.html"
+	context = {}
+	return render(request,template, context)
