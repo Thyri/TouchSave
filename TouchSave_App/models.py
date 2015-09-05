d24b721485cbda92f0fe9624f782e8f17a90fd5c
sklei2 <stk1151@rit.edu>
2015-09-05 14:46:22 -0400

    Fixing models.py
    Apparently I'm a moron, defining a class in python uses a lower case
    'c', max_length needs an underscore, and i need to define the XUser
    before using it...

--- TouchSave_App/models.py
+++ TouchSave_App/models.py
@@ -1,24 +1,11 @@
 from django.db import models
 
 # Create your models here.
-Class Allergies(models.Model):
-    allergy = models.CharField(maxlength=200)
-    user_with_allergy = models.ForeignKey(XUser)
 
-    def __str__(self):
-        return str(self.allergy)
-
-Class Comments(models.Model):
-    comment = models.CharField(maxlength=500)
-    users_commen = models.ForeignKey(XUser)
-
-    def _str__(self):
-       return str(self.comment)
-
-Class XUser(User):
+class XUser(User):
     date_of_birth = models.DateField("Date of Birth")
-    first_name = models.CharField(maxlength=200)
-    last_name = models.CharField(maxlength=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
     blood_type = (
         ('0_neg', '0-'),
         ('0_pos', '0+'),
@@ -36,5 +23,22 @@ Class XUser(User):
     def close(self):
        self.is_active = False
        self.save()
+
+
+
+class Allergies(models.Model):
+    allergy = models.CharField(max_length=200)
+    user_with_allergy = models.ForeignKey(XUser)
+
+    def __str__(self):
+        return str(self.allergy)
+
+class Comments(models.Model):
+    comment = models.CharField(max_length=500)
+    users_commen = models.ForeignKey(XUser)
+
+    def _str__(self):
+       return str(self.comment)
+
 
 
