bd18eab65a122045e71b3a2407d0454e2c9c0ba1
sklei2 <stk1151@rit.edu>
2015-09-05 16:08:27 -0400

    Moved urls to a different directory
    Cause, ya know. django directories.

--- TouchSave_App/views.py
+++ TouchSave_App/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponseRedirect
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.db import IntegrityError
+
 # Create your views here.
 def index(request):
 	template = "index.html"
